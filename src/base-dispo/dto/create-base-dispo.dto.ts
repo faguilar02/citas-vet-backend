96b3c3bd34beccaf34185400f9271b6f18878eb5
faguilar02 <daguilarv1@upao.edu.pe>
2025-04-16 21:21:33 -0500

    create base disponibility dto was implemented

--- src/base-dispo/dto/create-base-dispo.dto.ts
+++ src/base-dispo/dto/create-base-dispo.dto.ts
@@ -1,1 +1,18 @@
-export class CreateBaseDispoDto {}
+import { IsEnum, IsString, Matches } from "class-validator";
+import { BaseDispo } from "../entities/base-dispo.entity";
+
+export class CreateBaseDispoDto {
+
+    @IsEnum(BaseDispo)
+    @IsString()
+    dayOfWeek:string
+
+    @IsString()
+    @Matches(/^([0-1]\d|2[0-3]):([0-5]\d)$/)
+    startTime:string
+
+    @IsString()
+    @Matches(/^([0-1]\d|2[0-3]):([0-5]\d)$/)
+    endTime:string
+}
+
